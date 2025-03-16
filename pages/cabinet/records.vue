46670b00f115b442cfed986f397a79cc08390bf4
Dmitry <db@qd-team.ru>
2025-03-16 17:25:16 +0300

    refactor: remove select with rows and add card

--- pages/cabinet/records.vue
+++ pages/cabinet/records.vue
@@ -1,25 +1,28 @@
 <template>
 	<div class="card">
-		<DataTable
-			:value="products"
-			paginator
-			:rows="5"
-			:rows-per-page-options="[5, 10, 20, 50]"
-			table-style="min-width: 15rem"
-		>
-			<Column
-				field="name"
-				header="Клиент"
-			/>
-			<Column
-				field="doctor_name"
-				header="Имя Врача"
-			/>
-			<Column
-				field="date"
-				header="Дата приема"
-			/>
-		</DataTable>
+		<Card>
+			<template #content>
+				<DataTable
+					:value="products"
+					paginator
+					:rows="5"
+					table-style="min-width: 15rem"
+				>
+					<Column
+						field="name"
+						header="Клиент"
+					/>
+					<Column
+						field="doctor_name"
+						header="Имя Врача"
+					/>
+					<Column
+						field="date"
+						header="Дата приема"
+					/>
+				</DataTable>
+			</template>
+		</Card>
 	</div>
 </template>
 
