1486ff78c66002442dd9e67fee79d04584af4a8d
Dmitry <db@qd-team.ru>
2025-03-16 16:36:30 +0300

    feat: add table with records

---
+++ pages/cabinet/records.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="card">
+		<DataTable
+			:value="products"
+			paginator
+			:rows="5"
+			:rows-per-page-options="[5, 10, 20, 50]"
+			table-style="min-width: 15rem"
+		>
+			<Column
+				field="name"
+				header="Клиент"
+			/>
+			<Column
+				field="doctor_name"
+				header="Имя Врача"
+			/>
+			<Column
+				field="date"
+				header="Дата приема"
+			/>
+		</DataTable>
+	</div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { RecordService } from '~/service/RecordService.js';
+
+onMounted(() => {
+	RecordService.getRecordMini().then(data => (products.value = data));
+});
+
+const products = ref();
+</script>
